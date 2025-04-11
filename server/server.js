5e5e26693a02546bc9e2971a649257ad5a45678d
Vishal <vishaladhikari8604@gmail.com>
2025-04-11 18:00:55 +0530

    update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://similarity-opj8.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
